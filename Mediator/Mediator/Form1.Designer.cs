f57d762f4ddf1c004a713c4b8441ded7e494bcb7
Al3xTO <147641630+Al3xTO@users.noreply.github.com>
2024-04-08 22:38:18 +0200

    Add files via upload

---
+++ Mediator/Mediator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Mediator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "зуб",
+            "шлунок",
+            "спина"});
+            this.checkedListBox1.Location = new System.Drawing.Point(168, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(69, 49);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Доброго дня, у мене болить";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = ", мені потрібно записатися до лікаря.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Відправити";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "                          ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Лікар:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(102, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "                  ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Реєстратура:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 165);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Mediator/Mediator/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Mediator
+{
+    public partial class Form1 : Form
+    {
+        private Mediator mediator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMediator();
+            checkedListBox1.ItemCheck += CheckedListBox1_ItemCheck;
+            button1.Click += button1_Click;
+        }
+
+        private void InitializeMediator()
+        {
+            mediator = new Mediator();
+            mediator.RegisterColleague(new Dentist("Стоматолог"));
+            mediator.RegisterColleague(new Gastroenterologist("Гастроентеролог"));
+            mediator.RegisterColleague(new Traumatologist("Травматолог"));
+        }
+
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (i != e.Index)
+                {
+                    checkedListBox1.SetItemChecked(i, false);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var selectedItem = checkedListBox1.SelectedItem?.ToString();
+
+            if (selectedItem != null)
+            {
+                string specialist = mediator.Request(selectedItem);
+                if (specialist != null)
+                {
+                    label4.Text = specialist;
+                    label3.Text = "Записуємо вас до необхідного спеціаліста";
+                    label5.Text = "Запит прийнято";
+                }
+                else
+                {
+                    label3.Text = "Спеціаліст не знайдений";
+                    label5.Text = "";
+                }
+            }
+            else
+            {
+                label3.Text = "Виберіть запит";
+                label5.Text = "";
+            }
+        }
+
+    }
+
+    public class Mediator
+    {
+        private Dictionary<string, Colleague> colleagues = new Dictionary<string, Colleague>();
+
+        public void RegisterColleague(Colleague colleague)
+        {
+            colleagues[colleague.Name] = colleague;
+        }
+
+        public string Request(string request)
+        {
+            foreach (var colleague in colleagues.Values)
+            {
+                if (colleague.CanHandle(request))
+                {
+                    return colleague.Name;
+                }
+            }
+            return null;
+        }
+    }
+
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public string Name { get; }
+
+        public Colleague(string name)
+        {
+            Name = name;
+        }
+
+        public void SetMediator(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public abstract bool CanHandle(string request);
+    }
+
+    public class Dentist : Colleague
+    {
+        public Dentist(string name) : base(name) { }
+
+        public override bool CanHandle(string request)
+        {
+            return request == "зуб";
+        }
+    }
+
+    public class Gastroenterologist : Colleague
+    {
+        public Gastroenterologist(string name) : base(name) { }
+
+        public override bool CanHandle(string request)
+        {
+            return request == "шлунок";
+        }
+    }
+
+    public class Traumatologist : Colleague
+    {
+        public Traumatologist(string name) : base(name) { }
+
+        public override bool CanHandle(string request)
+        {
+            return request == "спина";
+        }
+    }
+}
